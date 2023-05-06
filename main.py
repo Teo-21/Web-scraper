e48c12634ccdbe8d191d45fd613587f565830bcf
Teo-21 <teodorpopovic95@gmail.com>
2023-05-06 02:35:35 +0200

    Only things that I've changed in this commit are: Added background color for each of the tabs moved code for tab creation and main tab layout and organized them into two separate instance methods That's basically it for now :)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from modules.WindowClass import *
 from modules.DriverClass import *
-from tkinter import ttk
+
+
 
 def main():
 
@@ -10,25 +11,16 @@ def main():
     programWindow = w.windowTk
     programWindow.title(w.windowTitle)
     programWindow.iconbitmap(w.windowIcon)
-    programWindow.geometry("800x600")
+    programWindow.geometry(w.windowGeometry)
 
 # Creating Tabs
 
-    tabs = ttk.Notebook(programWindow)
-    tabMain = ttk.Frame(tabs)
-    tabSettings = ttk.Frame(tabs)
-    tabAbout = ttk.Frame(tabs)
-    tabs.add(tabMain,text='Main')
-    tabs.add(tabSettings,text='Settings')
-    tabs.add(tabAbout,text='About')
-    tabs.pack(expand=1, fill='both')
+    w.createTabs()
+    
+# Main tab layout
 
-# Tabs content
+    w.mainTabElements()
 
-    ttk.Label(tabMain,text="Web scraper version 0.3.0").grid(column=0,row=0,padx=30,pady=30)
-    ttk.Label(tabSettings,text="Basic settings").grid(column=0,row=0,padx=30,pady=30)
-    ttk.Label(tabAbout,text="Info about this app will be added soon into this tab!").grid(column=0,row=0,padx=30,pady=30)
-   
     programWindow.mainloop()
     
 
--- modules/WindowClass.py
+++ modules/WindowClass.py
@@ -1,20 +1,46 @@
 from tkinter import *
-
+from tkinter import ttk
 
 class Window:
 
     windowTitle : str
     windowIcon : str
+    windowGeometry : str
     windowTk : object
-    
+    tabs : object
+    tabMain : object
+    tabSettings : object
+    tabAbout : object
+    bgColorOne : str
     
     def __init__(self):
 
         self.windowTitle = "Web scraper"
         self.windowIcon = "icons/web_icon.ico"
         self.windowTk = Tk()
-    
-   
+        self.windowGeometry = "800x600"
+        self.tabs = ttk.Notebook(self.windowTk)
+        self.bgColorOne = "#B0DAFF"
+        self.tabMain = Frame(self.tabs,bg= self.bgColorOne)
+        self.tabSettings = Frame(self.tabs,bg= self.bgColorOne)
+        self.tabAbout = Frame(self.tabs,bg= self.bgColorOne)
+       
 
+    def createTabs(self):
 
-   
+        self.tabMain
+        self.tabSettings
+        self.tabAbout
+        self.tabs.add(self.tabMain,text='Main') 
+        self.tabs.add(self.tabSettings,text='Settings')
+        self.tabs.add(self.tabAbout,text='About')
+        self.tabs.pack(expand=1, fill='both')
+
+    def mainTabElements(self):
+        
+        mainFrameOne = Frame(self.tabMain,width=250,height=280,bg="#19A7CE")
+        mainFrameOne.pack(side=LEFT,padx=10)
+        mainFrameTwo = Frame(self.tabMain,width=250,height=280,bg="#19A7CE")
+        mainFrameTwo.pack(side=LEFT,padx=10)
+        mainFrameThree = Frame(self.tabMain,width=250,height=280,bg="#19A7CE")
+        mainFrameThree.pack(side=LEFT,padx=10)
